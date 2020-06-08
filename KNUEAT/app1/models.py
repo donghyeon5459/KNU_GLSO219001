06031e754c21570f17d61f301849814636566724
hyeonjin <ki1029m@naver.com>
2020-06-08 16:39:29 +0900

    모델추가

--- KNUEAT/app1/models.py
+++ KNUEAT/app1/models.py
@@ -9,13 +9,29 @@ class Shop(models.Model):
     phone_number=models.CharField(max_length=225)
     open_time=models.CharField(max_length=225)
     close_time=models.CharField(max_length=225)
+    category=models.CharField(max_length=225)
     description=models.TextField()
-    owner=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     photo=models.ImageField(upload_to='images/')
+    owner=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+class Reservation(models.Model):
+    group_name=models.CharField(max_length=225)
+    time=models.CharField(max_length=225) #models.DateTimeField(auto_now_add=True)
+    requirements=models.CharField(max_length=225)
+    confirmed=models.IntegerField(default=0)
+    customer=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    shop=models.ForeignKey(Shop, on_delete=models.CASCADE, null=True)
 
 class Menu(models.Model):
+    name=models.CharField(max_length=225)
+    price=models.IntegerField()
     shop=models.ForeignKey(Shop, on_delete=models.CASCADE, null=True)
     
 class Review():
+    time=models.CharField(max_length=225)
+    rating=models.IntegerField()
+    comment=models.TextField()
     shop=models.ForeignKey(Shop, on_delete=models.CASCADE, null=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
 
