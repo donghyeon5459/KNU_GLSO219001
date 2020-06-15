d445d19c5fa7a2766aae8e63ef2ee86d0da45b8e
Donghyeon Kim <kevink3@naver.com>
2020-06-15 20:26:15 +0900

    view commit

--- KNUEAT/app1/views.py
+++ KNUEAT/app1/views.py
@@ -25,10 +25,11 @@ def home(request):
     return render(request,'home.html', {'shops':shop_list})
 
 def mypage_own(request):
+    user = request.user
     temp = user.profile.phonenumber
     number = temp[:3]+'-'+temp[3:7]+"-"+temp[7:]
     return render(request,'mypage_own.html' ,{'num':number})
-    return render(request,'mypage_own.html')
+
 
 def mypage_cus(request):
     return render(request,'mypage_cus.html')
