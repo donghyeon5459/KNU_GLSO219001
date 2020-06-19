0ee1f725ab10a8978b5ca98c217e9cbc104b0769
JeongUk Kim <jukim10101@gmail.com>
2020-06-19 18:11:11 +0900

    Revert "Test Automation set-up: tests.py"
    This reverts commit bf376096bc4d0ab1f01e351214cb7bf537eabd71.

--- KNUEAT/app1/tests.py
+++ KNUEAT/app1/tests.py
@@ -1,13 +1,3 @@
 from django.test import TestCase
-from selenium import webdriver
-import time
 
 # Create your tests here.
-
-if __name__ == '__main__':
-    driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
-    driver.get('http://127.0.0.1:8000/')
-
-    time.sleep(10)
-
-    driver.close()
