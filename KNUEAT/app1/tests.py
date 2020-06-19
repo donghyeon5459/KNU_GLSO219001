6aaa433deaa094db06f7bf335932de8d210c70f2
JeongUk Kim <jukim10101@gmail.com>
2020-06-19 23:07:30 +0900

    Test Case No.1 : login = Pass
    테스트 케이스 1번 통과~

--- KNUEAT/app1/tests.py
+++ KNUEAT/app1/tests.py
@@ -4,10 +4,39 @@ import time
 
 # Create your tests here.
 
-if __name__ == '__main__':
+customerID = "knueat"
+customerPW = "knueat"
+def setup():
     driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
     driver.get('http://127.0.0.1:8000/')
 
     time.sleep(10)
+    return driver
 
+def shutdown(driver):
+    
     driver.close()
+
+def login(driver):
+    loginbutton = driver.find_element_by_link_text("로그인")
+    loginbutton.click()
+
+    idBox = driver.find_element_by_name("userID")
+    idBox.clear()
+    idBox.send_keys(customerID)
+    
+    pwBox = driver.find_element_by_name("password")
+    pwBox.clear()
+    pwBox.send_keys(customerPW)
+
+    loginSend = driver.find_element_by_xpath("//input[@value='로그인']")
+    loginSend.click()
+
+    time.sleep(10)
+
+
+
+
+if __name__ == '__main__':
+    driver=setup()
+    login(driver)
